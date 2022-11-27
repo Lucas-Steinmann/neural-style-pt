98dd4cdcf50b570ffc8ae9e97f6db62309a8cd94
Lucas Steinmann <lucas@steinm.net>
2022-11-27 18:24:17 +0100

    Convert neural style script for usage with hydra.

--- neural_style.py
+++ neural_style.py
@@ -1,5 +1,7 @@
 import os
 import copy
+
+import hydra
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -8,55 +10,12 @@ import torchvision.transforms as transforms
 from PIL import Image
 from CaffeLoader import loadCaffemodel, ModelParallel
 
-import argparse
-parser = argparse.ArgumentParser()
-# Basic options
-parser.add_argument("-style_image", help="Style target image", default='examples/inputs/seated-nude.jpg')
-parser.add_argument("-style_blend_weights", default=None)
-parser.add_argument("-content_image", help="Content target image", default='examples/inputs/tubingen.jpg')
-parser.add_argument("-image_size", help="Maximum height / width of generated image", type=int, default=512)
-parser.add_argument("-gpu", help="Zero-indexed ID of the GPU to use; for CPU mode set -gpu = c", default=0)
-
-# Optimization options
-parser.add_argument("-content_weight", type=float, default=5e0)
-parser.add_argument("-style_weight", type=float, default=1e2)
-parser.add_argument("-normalize_weights", action='store_true')
-parser.add_argument("-normalize_gradients", action='store_true')
-parser.add_argument("-tv_weight", type=float, default=1e-3)
-parser.add_argument("-num_iterations", type=int, default=1000)
-parser.add_argument("-init", choices=['random', 'image'], default='random')
-parser.add_argument("-init_image", default=None)
-parser.add_argument("-optimizer", choices=['lbfgs', 'adam'], default='lbfgs')
-parser.add_argument("-learning_rate", type=float, default=1e0)
-parser.add_argument("-lbfgs_num_correction", type=int, default=100)
-
-# Output options
-parser.add_argument("-print_iter", type=int, default=50)
-parser.add_argument("-save_iter", type=int, default=100)
-parser.add_argument("-output_image", default='out.png')
-
-# Other options
-parser.add_argument("-style_scale", type=float, default=1.0)
-parser.add_argument("-original_colors", type=int, choices=[0, 1], default=0)
-parser.add_argument("-pooling", choices=['avg', 'max'], default='max')
-parser.add_argument("-model_file", type=str, default='models/vgg19-d01eb7cb.pth')
-parser.add_argument("-disable_check", action='store_true')
-parser.add_argument("-backend", choices=['nn', 'cudnn', 'mkl', 'mkldnn', 'openmp', 'mkl,cudnn', 'cudnn,mkl'], default='nn')
-parser.add_argument("-cudnn_autotune", action='store_true')
-parser.add_argument("-seed", type=int, default=-1)
-
-parser.add_argument("-content_layers", help="layers for content", default='relu4_2')
-parser.add_argument("-style_layers", help="layers for style", default='relu1_1,relu2_1,relu3_1,relu4_1,relu5_1')
-
-parser.add_argument("-multidevice_strategy", default='4,7,29')
-params = parser.parse_args()
-
-
 Image.MAX_IMAGE_PIXELS = 1000000000 # Support gigapixel images
 
 
-def main():
-    dtype, multidevice, backward_device = setup_gpu()
+@hydra.main(version_base=None, config_path=".", config_name="config")
+def main(params):
+    dtype, multidevice, backward_device = setup_gpu(params.backend, params.cudnn_autotune, params.gpu)
 
     cnn, layerList = loadCaffemodel(params.model_file, params.pooling, params.gpu, params.disable_check)
 
@@ -155,7 +114,7 @@ def main():
                 net.add_module(str(len(net)), layer)
 
     if multidevice:
-        net = setup_multi_device(net)
+        net = setup_multi_device(net, params.gpu, params.multidevice_strategy)
 
     # Capture content targets
     for i in content_losses:
@@ -258,51 +217,51 @@ def main():
 
         return loss
 
-    optimizer, loopVal = setup_optimizer(img)
+    optimizer, loopVal = setup_optimizer(img, params.optimizer, params.num_iterations, params.lbfgs_num_correction, params.learning_rate)
     while num_calls[0] <= loopVal:
          optimizer.step(feval)
 
 
 # Configure the optimizer
-def setup_optimizer(img):
-    if params.optimizer == 'lbfgs':
+def setup_optimizer(img, optimizer, num_iterations, lbfgs_num_correction, learning_rate):
+    if optimizer == 'lbfgs':
         print("Running optimization with L-BFGS")
         optim_state = {
-            'max_iter': params.num_iterations,
+            'max_iter': num_iterations,
             'tolerance_change': -1,
             'tolerance_grad': -1,
         }
-        if params.lbfgs_num_correction != 100:
-            optim_state['history_size'] = params.lbfgs_num_correction
+        if lbfgs_num_correction != 100:
+            optim_state['history_size'] = lbfgs_num_correction
         optimizer = optim.LBFGS([img], **optim_state)
         loopVal = 1
-    elif params.optimizer == 'adam':
+    elif optimizer == 'adam':
         print("Running optimization with ADAM")
-        optimizer = optim.Adam([img], lr = params.learning_rate)
-        loopVal = params.num_iterations - 1
+        optimizer = optim.Adam([img], lr = learning_rate)
+        loopVal = num_iterations - 1
     return optimizer, loopVal
 
 
-def setup_gpu():
+def setup_gpu(backend, cudnn_autotune, gpu):
     def setup_cuda():
-        if 'cudnn' in params.backend:
+        if 'cudnn' in backend:
             torch.backends.cudnn.enabled = True
-            if params.cudnn_autotune:
+            if cudnn_autotune:
                 torch.backends.cudnn.benchmark = True
         else:
             torch.backends.cudnn.enabled = False
 
     def setup_cpu():
-        if 'mkl' in params.backend and 'mkldnn' not in params.backend:
+        if 'mkl' in backend and 'mkldnn' not in backend:
             torch.backends.mkl.enabled = True
-        elif 'mkldnn' in params.backend:
+        elif 'mkldnn' in backend:
             raise ValueError("MKL-DNN is not supported yet.")
-        elif 'openmp' in params.backend:
+        elif 'openmp' in backend:
             torch.backends.openmp.enabled = True
 
     multidevice = False
-    if "," in str(params.gpu):
-        devices = params.gpu.split(',')
+    if "," in str(gpu):
+        devices = gpu.split(',')
         multidevice = True
 
         if 'c' in str(devices[0]).lower():
@@ -313,20 +272,20 @@ def setup_gpu():
             setup_cuda()
         dtype = torch.FloatTensor
 
-    elif "c" not in str(params.gpu).lower():
+    elif "c" not in str(gpu).lower():
         setup_cuda()
-        dtype, backward_device = torch.cuda.FloatTensor, "cuda:" + str(params.gpu)
+        dtype, backward_device = torch.cuda.FloatTensor, "cuda:" + str(gpu)
     else:
         setup_cpu()
         dtype, backward_device = torch.FloatTensor, "cpu"
     return dtype, multidevice, backward_device
 
 
-def setup_multi_device(net):
-    assert len(params.gpu.split(',')) - 1 == len(params.multidevice_strategy.split(',')), \
+def setup_multi_device(net, gpu, multidevice_strategy):
+    assert len(gpu.split(',')) - 1 == len(multidevice_strategy.split(',')), \
       "The number of -multidevice_strategy layer indices minus 1, must be equal to the number of -gpu devices."
 
-    new_net = ModelParallel(net, params.gpu, params.multidevice_strategy)
+    new_net = ModelParallel(net, gpu, multidevice_strategy)
     return new_net
 
 
